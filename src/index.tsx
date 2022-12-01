24994201bf144ea35c71cadaf9b47d4f96f91aae
Al-Meqdad Jabi <mqdad1aljabi@gmail.com>
2022-12-01 06:57:37 +0200

    fix:errors

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,19 @@ import "./Index.css";
 import store from "./Store";
 import { Provider } from "react-redux";
 import AllFather from "./AllFather";
-import Login from "./Login/Login";
-import { AuthProvider } from "./context/AuthProvider";
 
 const Index = () => {
   return (
     <StrictMode>
-      <AuthProvider>
+
         <Provider store={store}>
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<AllFather />} />
-              <Route path="Login" element={<Login />}></Route>
             </Routes>
           </BrowserRouter>
         </Provider>
-      </AuthProvider>
+ 
     </StrictMode>
   );
 };
